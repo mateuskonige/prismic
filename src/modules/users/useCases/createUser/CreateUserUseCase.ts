4ce1e8abe4f19e98f76ef20819343056fc22c328
Mateus Reis <you@example.com>
2022-08-31 15:19:21 -0300

    Create User

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,28 @@
+import { User } from "@prisma/client";
+import { prisma } from "../../../../prisma/client";
+import { CreateUserDTO } from "../../dtos/CreateUserDTO";
+
+export class CreateUserUseCase {
+  async execute({name, email}: CreateUserDTO): Promise<User> {
+    // Verificar se o usuário já existe
+    const userAlreadyExists = await prisma.user.findUnique({
+      where: {
+        email
+      }
+    })
+
+    // Criar o usuário
+    if(userAlreadyExists) {
+      // erro
+    }
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email
+      }
+    })
+
+    return user;
+  }
+}
